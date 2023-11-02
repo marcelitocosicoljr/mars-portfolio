1ce1869266e3fd8a27a7fb36949e29c6284887c6
Marcelito Cosicol <mcosicoljr@gmail.com>
2023-11-02 13:02:37 +0800

    updated projects

--- app/components/views/home/Contact.tsx
+++ app/components/views/home/Contact.tsx
@@ -7,25 +7,25 @@ const Contact = () => {
     <div className='w-full flex flex-wrap justify-center gap-8 items-center py-4'>
 
         <a data-aos="fade-up" href='tel:+639397509659' target='_blank' className='w-[180px] h-[170px] rounded-md shadow-xl bg-white flex flex-col justify-center items-center gap-6 text-theme-blue hover:text-theme-orange hover:scale-110 transition duration-150'>
-            <Icon icon='tdesign:call' width={50} className='' />
+            <Icon icon='tdesign:call' width={50} />
             <p className=' text-[12px] font-medium  items-center'>
             (+63) 939 750 9659
             </p>
         </a>
         <a data-aos="fade-up" href='mailto:mcosicoljr@gmail.com' target='_blank' className='w-[180px] h-[170px] rounded-md shadow-xl bg-white flex flex-col justify-center items-center gap-6  text-theme-blue hover:text-theme-orange hover:scale-110 transition duration-150'>
-            <Icon icon='ic:outline-email' width={50} className='text-theme-blue' />
+            <Icon icon='ic:outline-email' width={50} />
             <p className=' text-[12px] font-medium  items-center'>
             Email Me
             </p>
         </a>
         <a data-aos="fade-up" href='https://www.facebook.com/marcelito.cos' target='_blank'  className='w-[180px] h-[170px] rounded-md shadow-xl bg-white flex flex-col justify-center items-center gap-6  text-theme-blue hover:text-theme-orange hover:scale-110 transition duration-150'>
-            <Icon icon='fa-brands:facebook' width={50} className='text-theme-blue' />
+            <Icon icon='fa-brands:facebook' width={50}  />
             <p className=' text-[12px] font-medium  items-center'>
             View my Facebook
             </p>
         </a>
         <a data-aos="fade-up" href='https://www.linkedin.com/in/marcelito-cosicol-19288b22b/' target='_blank'  className='w-[180px] h-[170px] rounded-md shadow-xl bg-white flex flex-col justify-center items-center gap-6  text-theme-blue hover:text-theme-orange hover:scale-110 transition duration-150'>
-            <Icon icon='la:linkedin' width={50} className='text-theme-blue' />
+            <Icon icon='la:linkedin' width={50}  />
             <p className=' text-[12px] font-medium  items-center'>
             View my Linkedin
             </p>
--- app/components/views/home/Projects.tsx
+++ app/components/views/home/Projects.tsx
@@ -11,18 +11,22 @@ const Projects = () => {
         <Project image='/assets/images/projects/fksite.png' title='Farm Konekt Website' tasks={["UI/UX Lead", "Maintained Web App"]} link='https://farmkonek.com/' />
 
         <Project image='/assets/images/projects/studportal.png' title='Student Portal' tasks={["Designed UI/UX", "Developed Web App"]} />
+        <Project image='/assets/images/projects/psuweb.png' title='PSU Registrar Website' tasks={["Designed UI/UX", "Developed Web App"]} link='https://ourpsu.com/' />
 
         <Project image='/assets/images/projects/istopp.png' title='ISTOPP Web App' tasks={["(Information System on Transport Operations in Puerto Princesa)", "Developed Web App","Integrate Data"]} />
 
-        <Project image='/assets/images/projects/grate.png' title='Grate New Castle' tasks={["Developed Web App", "Maintained Web App"]} link='https://gratenewcastle.co.uk/' />
+        <Project image='/assets/images/projects/house.png' title='Housination' tasks={["Developed Web App"]} link='https://housination.com/' />
+
+        <Project image='/assets/images/projects/grate.png' title='Grate New Castle' tasks={["Developed Web App", "Maintained Web App"]}  />
 
         <Project image='/assets/images/projects/scott.png' title='Scottbros' tasks={["Maintained Web App"]} link='https://scottbros.com/' />
-        <Project image='/assets/images/projects/house.png' title='Housination' tasks={["Developed Web App"]} link='https://housination.com/' />
+
 
         <SmallProject image='/assets/images/projects/4.png' title='TL Competitions' tasks={["Developed Web App"]}  />
         <SmallProject image='/assets/images/projects/5.png' title='Australia Workers' tasks={["Developed Web App"]} link='https://australiaworkers.com/'  />
 
         <Project image='/assets/images/projects/next.png' title='NextGen Sports Camp' tasks={["Developed Web App", "Maintained Web App"]} link='https://nextgensportscamps.co.uk/clubs/' />
+
         <Project image='/assets/images/projects/babylon.png' title='Babylon Durham' tasks={["Maintained Web App"]} link='https://babylondurham.com/' />
         <Project image='/assets/images/projects/basilico.png' title='Basilico' tasks={["Developed Web App"]} link='https://basilico.co.uk/' />
 
@@ -49,6 +53,7 @@ const Projects = () => {
 
         <MobileProject image='/assets/images/projects/fkmobile.png' title='Farm Konekt Web App' tasks={["Developed Web App"]}  />
         <MobileProject image='/assets/images/projects/student.jpg' title='PSU Student Portal' tasks={["Developed Web App"]}  />
+        <MobileProject image='/assets/images/projects/psuwebmobile.png' title='PSU Registrar Portal' tasks={["Developed Web App"]}  />
 
 
 
