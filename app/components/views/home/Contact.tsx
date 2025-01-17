76a9c7ad39228947216f586a9118fa22a8879290
Marcelito Cosicol <78013167+marcelitocosicoljr@users.noreply.github.com>
2025-01-17 10:34:28 +0800

    Update Contact.tsx

--- app/components/views/home/Contact.tsx
+++ app/components/views/home/Contact.tsx
@@ -6,7 +6,6 @@ const Contact = () => {
   return (
     <div className="w-full flex flex-wrap justify-center gap-8 items-center py-4">
       <a
-        data-aos="fade-up"
         href="tel:+639397509659"
         target="_blank"
         className="w-[180px] h-[170px] rounded-md shadow-xl bg-white flex flex-col justify-center items-center gap-6 text-theme-blue hover:text-theme-orange hover:scale-110 transition duration-150"
@@ -17,7 +16,7 @@ const Contact = () => {
         </p>
       </a>
       <a
-        data-aos="fade-up"
+     
         href="mailto:mcosicoljr@gmail.com"
         target="_blank"
         className="w-[180px] h-[170px] rounded-md shadow-xl bg-white flex flex-col justify-center items-center gap-6  text-theme-blue hover:text-theme-orange hover:scale-110 transition duration-150"
@@ -26,7 +25,7 @@ const Contact = () => {
         <p className=" text-[12px] font-medium  items-center">Email Me</p>
       </a>
       <a
-        data-aos="fade-up"
+    
         href="https://www.facebook.com/mcosicoljr"
         target="_blank"
         className="w-[180px] h-[170px] rounded-md shadow-xl bg-white flex flex-col justify-center items-center gap-6  text-theme-blue hover:text-theme-orange hover:scale-110 transition duration-150"
@@ -37,7 +36,7 @@ const Contact = () => {
         </p>
       </a>
       <a
-        data-aos="fade-up"
+  
         href="https://www.linkedin.com/in/marcelito-cosicol-19288b22b/"
         target="_blank"
         className="w-[180px] h-[170px] rounded-md shadow-xl bg-white flex flex-col justify-center items-center gap-6  text-theme-blue hover:text-theme-orange hover:scale-110 transition duration-150"
